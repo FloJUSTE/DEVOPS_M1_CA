e1370d3d62da3941eb489cb98c3b4064e04e79e3
root <root@linuxsrv07.home.local>
2020-11-25 14:55:33 +0100

    skeleton + tests

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-git remote add cleverapps
-https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_18bf0d13-bc68-4e6c-b8a4-27325daf78ef.git
+git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_18bf0d13-bc68-4e6c-b8a4-27325daf78ef.git
 git --verbose --force push cleverapps master
